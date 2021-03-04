34d9acfa92e063a1be831ff59f2c809fa21b90fd
RaifIT <raif5it@gmail.com>
2021-03-04 14:25:41 +0300

    First Naive solution

---
+++ 001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(naive(10));
+            Console.WriteLine(naive(1000));
+        }
+        private static double naive(int maxNum){
+            return sum(multiples(maxNum));
+        }
+
+        private static double sum(List<int> input){
+            int sum = 0;
+                foreach (int i in input)
+                {
+                    sum += i;
+                }
+            return sum;
+        }
+        private static List<int> multiples(int maxNum){
+            List<int> multiplesList = new List<int>();
+            for (int i = 0; i < maxNum; i++)
+            {
+                if(i%3 == 0 || i%5 == 0){
+                    multiplesList.Add(i);
+                }
+            }
+            return multiplesList;
+        }
+    }
+}
