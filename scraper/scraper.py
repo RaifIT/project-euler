7d7f7cbd886dff24a3d60cb18107b818f8e67268
Raif IT <raif5it@gmail.com>
2021-04-07 19:46:51 +0300

    Added function to save dictionary in a JSON

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,8 @@
 import os
 import requests
+import json
 from bs4 import BeautifulSoup
+
 
 def run():
     markdown_dict = {}                                                          # Markdown Dictionary
@@ -8,20 +10,14 @@ def run():
     for p in range(from_problem, to_problem+1):
         scrape(markdown_dict, p)                                                # Scrape the webpage and add to dict
         save_md_to_file(markdown_dict, p)                                       # Save problem MD in problem folder
+    save_dict_to_json(markdown_dict)                                            # Save dictionary to a JSON
+
 
 def scrape(markdown_dict, problem_id):
     url = 'https://projecteuler.net/problem={}'.format(problem_id)              # URL to scrape
     page = requests.get(url).content                                            # HTML Content
     soup = BeautifulSoup(page, 'html.parser')                                   # Soup
     filter_soup(soup, markdown_dict, problem_id)                                # Filter the soup and create the MD
-
-
-def save_md_to_file(markdown_dict, problem_id):
-    path = '../{}/'.format(str(problem_id).zfill(3))                            # Directory Path
-    if not os.path.isdir(path):                                                 # Check if directory exists
-        os.mkdir(path)                                                          # Create it if not
-    with open(path + 'README.MD', 'w+') as file:                                # Create/Open README.MD
-        file.write(markdown_dict[problem_id]['md'])                             # Write problem description to file
 
 
 def filter_soup(soup, markdown_dict, problem_id):
@@ -33,6 +29,19 @@ def filter_soup(soup, markdown_dict, problem_id):
     details = {'title': title,                                                  # Create new entry
             'subtitle': subtitle, 'description': description, 'md': markdown_text}
     markdown_dict[problem_id] = details                                         # Add entry to dictionary
+
+
+def save_md_to_file(markdown_dict, problem_id):
+    path = '../{}/'.format(str(problem_id).zfill(3))                            # Directory Path
+    if not os.path.isdir(path):                                                 # Check if directory exists
+        os.mkdir(path)                                                          # Create it if not
+    with open(path + 'README.MD', 'w+') as file:                                # Create/Open README.MD
+        file.write(markdown_dict[problem_id]['md'])                             # Write problem description to file
+
+
+def save_dict_to_json(markdown_dict):
+    with open('dictionary.json', 'w+') as file:                                 # Create/Open dictionary.json
+        json.dump(markdown_dict, file, indent=5)                                # Write dictionary to JSON
 
 
 run()
